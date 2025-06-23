5798ef5273200913be7ee27194dde638e17a99dc
ericorps1 <ericorps1@gmail.com>
2025-06-23 12:25:04 -0600

    obtenerPaymentIntent added

--- src/routes/pagos.routes.js
+++ src/routes/pagos.routes.js
@@ -2,13 +2,15 @@
 import { Router } from "express";
 import { 
     crearPaymentIntent, 
+    obtenerPaymentIntent,
     webhookStripe 
-} from "../controllers/pagos.controller.js";
+} from "../controllers/stripe.controller.js";
 
 const router = Router();
 
 // Rutas para Stripe
 router.post('/crear-payment-intent', crearPaymentIntent);
-router.post('/webhook-stripe2', webhookStripe); // ✅ AGREGAR ESTA LÍNEA
+router.post('/obtener-payment-intent', obtenerPaymentIntent); // ✅ NUEVA RUTA
+router.post('/webhook-stripe2', webhookStripe);
 
 export default router;
